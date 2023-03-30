a8942af88be2c77b6044de01e468c12f3a6ec7fc
Andrii <opopadinets43@gmail.com>
2023-03-30 16:57:08 +0200

    added delay and options for detected_lanugages manually

--- bot.py
+++ bot.py
@@ -7,8 +7,8 @@ import pytesseract
 import googletrans
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #  
-
 from PIL import Image
+from typing import Dict, Any
 from PyPDF4 import PdfFileReader, PdfFileWriter
 from pdf2image import convert_from_bytes
 from reportlab.lib.pagesizes import letter, landscape
@@ -16,7 +16,7 @@ from reportlab.pdfgen import canvas
 from telegram import ReplyKeyboardMarkup
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-from googletrans import Translator
+from googletrans import Translator, LANGUAGES
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CallbackQueryHandler
 from reportlab.pdfbase import pdfmetrics
@@ -46,8 +46,9 @@ def start(update: Update, context: CallbackContext) -> None:
         "Send me an image, text, or PDF to translate it to your chosen language."
         "3. Type '/menu_output_format' to choose a format of answer from bot (text of pdf file)\n"
         "4. Type '/help' to show all commands"
-        # reply_markup=language_keyboard(),
     )
+    reply_markup=language_keyboard()
+    
 
 def help(update: Update, context: CallbackContext) -> None:
     help_text = (
@@ -77,19 +78,69 @@ def format_keyboard() -> InlineKeyboardMarkup:
     ]
     return InlineKeyboardMarkup(keyboard)
 
+def options_keyboard() -> InlineKeyboardMarkup:
+    keyboard = [
+        [
+            InlineKeyboardButton("English", callback_data="en"),
+            InlineKeyboardButton("Russian", callback_data="ru"),
+            InlineKeyboardButton("Dutch", callback_data="nl"),
+        ],
+        [
+            InlineKeyboardButton("German", callback_data="de"),
+            InlineKeyboardButton("Spanish", callback_data="es"),
+            InlineKeyboardButton("Italian", callback_data="it"),
+        ],
+        [
+            InlineKeyboardButton("Arabic", callback_data="ar"),
+            InlineKeyboardButton("French", callback_data="fr"),
+            InlineKeyboardButton("Norwegian", callback_data="no"),
+        ]
+    ]
+    return InlineKeyboardMarkup(keyboard)
+
+def confirmation_keyboard(target_language: str) -> InlineKeyboardMarkup:
+    keyboard = [
+        [
+            InlineKeyboardButton("yes", callback_data="yes"),
+            InlineKeyboardButton("no", callback_data="no"),
+        ],
+        
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    # Set the target language as the detected language if the user has already chosen a language
+    if target_language:
+        for row in reply_markup.inline_keyboard[1:]:
+            for button in row:
+                if button.callback_data == target_language:
+                    button.text = f"✅ {button.text}"
+
+    return reply_markup
+
+
+def handle_language_confirmation(update: Update, context: CallbackContext, detected_language: str, img_or_images, target_language: str) -> None:
+    message = update.message.reply_text(
+        f"Detected language: {detected_language}. Is this correct?",
+        reply_markup=confirmation_keyboard(target_language),
+    )
+
+    context.user_data["detected_language"] = detected_language
+    context.user_data["img_or_images"] = img_or_images
+    context.user_data["confirmation_message_id"] = message.message_id
+
+
 def ocr_translate(img, target_language):
-    text = pytesseract.image_to_string(img)
+    text = pytesseract.image_to_string(img, lang='ukr')
 
     # Add this check to validate the target language code
     if target_language not in googletrans.LANGUAGES:
         return f"Error: Invalid target language '{target_language}'. Please choose a valid language code."
 
     try:
-        lang = translator.detect(text).lang
-        translation = translator.translate(text, dest=target_language).text
-        return f'Translation from {lang} to {target_language}: {translation}'
+        translation = translator.translate(text, src='uk', dest=target_language).text
+        return f'Translation from ukr to {target_language}: {translation}'
     except TypeError:
-        return "Error: Unable to detect language or translate text. Please try again."
+        return "Error: Unable to translate text. Please try again."
 
 
 def ocr_file(update: Update, context: CallbackContext) -> None:
@@ -113,6 +164,27 @@ def handle_ocr_file(update: Update, context: CallbackContext) -> None:
 
     # Remove the message handler after processing the OCR file
     context.dispatcher.remove_handler(MessageHandler(Filters.all, handle_ocr_file, pass_user_data=True))
+    
+def process_translation(update: Update, context: CallbackContext) -> None:
+    target_language = context.user_data.get("target_language", "en")
+    img_or_images = context.user_data["img_or_images"]
+
+    if isinstance(img_or_images, Image.Image):
+        lang, text = ocr_translate(img_or_images, target_language)
+        translation = translator.translate(text, dest=target_language).text
+        reply_text = f'Translation from {lang} to {target_language}: {translation}'
+        send_translation(update, context, reply_text, filename="image_translation.pdf")
+    elif isinstance(img_or_images, list):
+        translations = []
+
+        for idx, img in enumerate(img_or_images):
+            lang, text = ocr_translate(img, target_language)
+            translation = translator.translate(text, dest=target_language).text
+            translations.append(f'Page {idx + 1}:\nTranslation from {lang} to {target_language}: {translation}')
+
+        reply_text = "\n\n".join(translations)
+        send_translation(update, context, reply_text, filename="all_translated_pages.pdf")
+
     
 def handle_format_selection(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
@@ -143,13 +215,46 @@ def handle_language_selection(update: Update, context: CallbackContext) -> None:
     query.edit_message_text(
         text=f"Selected language: {target_language}. Now send me an image, text, or PDF to translate.")
 
+def handle_confirmation(update: Update, context: CallbackContext) -> None:
+    confirmed = query.data == "yes"
+    rejected = query.data == "no"
+    query = update.callback_query
+    query.answer()
+
+
+    if confirmed:
+        target_language = context.user_data["target_language"]
+        img_or_images = context.user_data["img_or_images"]
+        detected_language = context.user_data["detected_language"]
+
+        # # Set the target_language as the detected_language if the user has not chosen a language yet
+        # if not target_language:
+        #     target_language = detected_language
+
+        # Translate the PDF/image
+        process_translation(update, context)
+    elif rejected:
+        target_language = context.user_data["target_language"]
+        img_or_images = context.user_data["img_or_images"]
+        query.edit_message_text(
+        text=language_keyboard()
+    )
+        
+
+    else:
+        update.effective_chat.send_message("Please choose the correct language:", reply_markup=language_keyboard())
+
+    # Remove the confirmation message
+    confirmation_message_id = context.user_data["confirmation_message_id"]
+    update.effective_chat.delete_message(confirmation_message_id)
+
 
 
 def send_translation(update: Update, context: CallbackContext, reply_text: str, filename="translation.pdf"):
     output_format = context.user_data.get("output_format", "text")
 
     if output_format == "text":
-        update.message.reply_text(reply_text)
+        update.effective_message.reply_text(reply_text)
     else:
         send_pdf(update, context, reply_text, filename=filename)
 
@@ -179,20 +284,17 @@ def handle_text(update: Update, context: CallbackContext) -> None:
     lang = translator.detect(text).lang
     translation = translator.translate(text, dest=target_language).text
 
-    page_number = context.user_data.get("page_number", 1)
-    reply_text = f'Translation from {lang} to {target_language}, Page {page_number}: {translation}'
+    reply_text = f'Translation from {lang}: {translation}'
     send_translation(update, context, reply_text)
-    context.user_data["page_number"] = page_number + 1
-
 
 def handle_image(update: Update, context: CallbackContext) -> None:
     image_url = update.message.photo[-1].get_file().file_path
     response = requests.get(image_url)
     img = Image.open(io.BytesIO(response.content))
     target_language = context.user_data.get("target_language", "en")
-    reply_text = ocr_translate(img, target_language)
+    detected_language, _ = ocr_translate(img, target_language)
 
-    send_translation(update, context, reply_text, filename="image_translation.pdf")
+    handle_language_confirmation(update, context, detected_language, img)
 
 def handle_document(update: Update, context: CallbackContext) -> None:
     document = update.message.document
@@ -202,46 +304,13 @@ def handle_document(update: Update, context: CallbackContext) -> None:
         file.download(out=pdf_data)
         images = convert_from_bytes(pdf_data.getvalue())
 
-        translations = []
+        # Use the first page to detect the language
+        target_language = context.user_data.get("target_language", "en")
+        detected_language, _ = ocr_translate(images[0], target_language)
 
-        for idx, img in enumerate(images):
-            target_language = context.user_data.get("target_language", "en")
-            translation_text = ocr_translate(img, target_language)
-            translations.append(translation_text)
-
-        send_merged_pdf(update, context, translations, filename="all_translated_pages.pdf")
+        handle_language_confirmation(update, context, detected_language, images, target_language)  # pass target_language to handle_language_confirmation
     else:
         update.message.reply_text('Unsupported document type.')
-
-def send_merged_pdf(update, context, texts, filename="translation.pdf"):
-    buffer = io.BytesIO()
-    pdf_writer = PdfFileWriter()
-
-    for text in texts:
-        page_buffer = io.BytesIO()
-        c = canvas.Canvas(page_buffer, pagesize=landscape(letter))
-        c.setFont("FreeSans", 12)
-
-        max_width = 750
-        lines = textwrap.wrap(text, width=100)
-
-        x, y = 50, 500
-        for line in lines:
-            c.drawString(x, y, line)
-            y -= 15
-
-        add_watermark(c)
-        c.showPage()
-        c.save()
-        page_buffer.seek(0)
-
-        page_pdf = PdfFileReader(page_buffer)
-        pdf_writer.addPage(page_pdf.getPage(0))
-
-    with buffer:
-        pdf_writer.write(buffer)
-        buffer.seek(0)
-        context.bot.send_document(chat_id=update.effective_chat.id, document=buffer, filename=filename)
 
 def menu_output_format(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("Choose an output format:", reply_markup=format_keyboard())
@@ -264,6 +333,7 @@ def main() -> None:
     dispatcher.add_handler(MessageHandler(Filters.document & ~Filters.command, handle_document))
     dispatcher.add_handler(CommandHandler("menu_output_format", menu_output_format))
     dispatcher.add_handler(CallbackQueryHandler(handle_format_selection, pattern="^(text|pdf)$"))
+    dispatcher.add_handler(CallbackQueryHandler(handle_confirmation, pattern="^(yes|no)$"))
     dispatcher.add_handler(CommandHandler("menu_language", menu_language))
 
     updater.start_polling()
@@ -272,3 +342,7 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
+    
+    
+    
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #   
