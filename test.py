bb15289a29510eb633a30b75f9816cdcc25c23a0
Andrii <opopadinets43@gmail.com>
2023-07-20 22:16:10 +0200

    get ready before deploy

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ import textwrap
 import constants as consts
 import pytesseract
 import googletrans
-import pandas as pd
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #  
 
@@ -22,10 +21,6 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CallbackQueryHandler
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
-from openpyxl import Workbook
-from requests.exceptions import RequestException
-
-
 
 TELEGRAM_API_KEY = consts.TOKEN
 OPENAI_API_KEY = consts.API_KEY
@@ -51,13 +46,6 @@ def start(update: Update, context: CallbackContext) -> None:
         "3. Type '/help' to show all commands"
         # reply_markup=language_keyboard(),
     )
-
-def error_handler(update: Update, context: CallbackContext) -> None:
-    logger.error(f"Update {update} caused error {context.error}")
-
-    # Notify the user that an error occurred
-    if update and update.effective_message:
-        update.effective_message.reply_text("An error occurred. Please try again.")
 
 def help(update: Update, context: CallbackContext) -> None:
     help_text = (
@@ -88,6 +76,7 @@ def format_keyboard() -> InlineKeyboardMarkup:
 def ocr_translate(img, target_language):
     text = pytesseract.image_to_string(img)
 
+    # Add this check to validate the target language code
     if target_language not in googletrans.LANGUAGES:
         return f"Error: Invalid target language '{target_language}'. Please choose a valid language code."
 
@@ -97,8 +86,6 @@ def ocr_translate(img, target_language):
         return f'Translation from {lang} to {target_language}: {translation}'
     except TypeError:
         return "Error: Unable to detect language or translate text. Please try again."
-    except RequestException:
-        return "Error: Something went wrong with the translation. Please try again."
 
 
 def ocr_file(update: Update, context: CallbackContext) -> None:
@@ -108,20 +95,26 @@ def ocr_file(update: Update, context: CallbackContext) -> None:
     # Use the message handler to handle OCR files
     context.dispatcher.add_handler(MessageHandler(Filters.all, handle_ocr_file, pass_user_data=True))
 
-def handle_ocr_file(update: Update, context: CallbackContext) -> None:
-    if update.message.text == "Cancel":
-        update.message.reply_text("Canceled.")
-        return
+def processing_message(update: Update, context: CallbackContext) -> int:
+    return context.bot.send_message(chat_id=update.message.chat_id, text="Processing...").message_id
 
-    if update.message.photo:
-        handle_image(update, context)
-    elif update.message.document and update.message.document.file_name.lower().endswith('.pdf'):
-        handle_document(update, context)
-    else:
-        update.message.reply_text('Please send a valid image or PDF file.')
+def delete_processing_message(update: Update, context: CallbackContext, message_id: int) -> None:
+    context.bot.delete_message(chat_id=update.message.chat_id, message_id=message_id)
 
-    # Remove the message handler after processing the OCR file
-    context.dispatcher.remove_handler(MessageHandler(Filters.all, handle_ocr_file, pass_user_data=True))
+def ocr_translate(img, target_language, update: Update, context: CallbackContext):
+    processing_msg_id = processing_message(update, context)
+    text = pytesseract.image_to_string(img)
+    if target_language not in googletrans.LANGUAGES:
+        delete_processing_message(update, context, processing_msg_id)
+        return f"Error: Invalid target language '{target_language}'. Please choose a valid language code."
+    try:
+        lang = translator.detect(text).lang
+        translation = translator.translate(text, dest=target_language).text
+        delete_processing_message(update, context, processing_msg_id)
+        return f'Translation from {lang} to {target_language}: {translation}'
+    except TypeError:
+        delete_processing_message(update, context, processing_msg_id)
+        return "Error: Unable to detect language or translate text. Please try again."
     
 def handle_format_selection(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
@@ -133,29 +126,6 @@ def handle_format_selection(update: Update, context: CallbackContext) -> None:
     query.edit_message_text(
         text=f"Selected output format: {output_format}. Now send me an image, text, or PDF to translate."
     )
-    
-def handle_excel(update: Update, context: CallbackContext) -> None:
-    document = update.message.document
-    print(document.mime_type)
-    if document.mime_type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
-        file = context.bot.get_file(update.message.document.file_id)
-        excel_data = io.BytesIO()
-        file.download(out=excel_data)
-        df = pd.read_excel(excel_data)
-        print(df.head())
-
-        target_language = context.user_data.get("target_language", "en")
-        translated_df = df.applymap(lambda x: translator.translate(str(x), dest=target_language).text if (pd.notnull(x) and isinstance(x, str)) else x)
-
-        buffer = io.BytesIO()
-        with pd.ExcelWriter(buffer, engine='openpyxl') as writer:
-            translated_df.to_excel(writer, index=False)
-
-        buffer.seek(0)
-        context.bot.send_document(chat_id=update.effective_chat.id, document=buffer, filename="translated_excel.xlsx")
-    else:
-        update.message.reply_text('Unsupported document type.')
-
 
 def add_watermark(c, watermark_text="Machine translation", x=70, y=70, font="Helvetica", font_size=50, opacity=0.1):
     c.saveState()
@@ -206,11 +176,12 @@ def send_pdf(update, context, text, filename="translation.pdf"):
 
 
 def handle_text(update: Update, context: CallbackContext) -> None:
+    processing_msg_id = processing_message(update, context)
     text = update.message.text
     target_language = context.user_data.get("target_language", "en")
     lang = translator.detect(text).lang
     translation = translator.translate(text, dest=target_language).text
-
+    delete_processing_message(update, context, processing_msg_id)
     page_number = context.user_data.get("page_number", 1)
     reply_text = f'Translation from {lang} to {target_language}, Page {page_number}: {translation}'
     send_translation(update, context, reply_text)
@@ -218,21 +189,17 @@ def handle_text(update: Update, context: CallbackContext) -> None:
 
 
 def handle_image(update: Update, context: CallbackContext) -> None:
-    try:
-        image_url = update.message.photo[-1].get_file().file_path
-        response = requests.get(image_url)
-    except RequestException as e:
-        logger.error(e)
-        update.message.reply_text('There was a problem getting the image. Please try again.')
-        return
-
+    processing_msg_id = processing_message(update, context)
+    image_url = update.message.photo[-1].get_file().file_path
+    response = requests.get(image_url)
     img = Image.open(io.BytesIO(response.content))
     target_language = context.user_data.get("target_language", "en")
-    reply_text = ocr_translate(img, target_language)
+    reply_text = ocr_translate(img, target_language, update, context)
+    delete_processing_message(update, context, processing_msg_id)
     send_translation(update, context, reply_text, filename="image_translation.pdf")
 
-
 def handle_document(update: Update, context: CallbackContext) -> None:
+    processing_msg_id = processing_message(update, context)
     document = update.message.document
     if document.file_name.lower().endswith('.pdf'):
         file = context.bot.get_file(update.message.document.file_id)
@@ -244,11 +211,12 @@ def handle_document(update: Update, context: CallbackContext) -> None:
 
         for idx, img in enumerate(images):
             target_language = context.user_data.get("target_language", "en")
-            translation_text = ocr_translate(img, target_language)
+            translation_text = ocr_translate(img, target_language, update, context)
             translations.append(translation_text)
-
+        delete_processing_message(update, context, processing_msg_id)
         send_merged_pdf(update, context, translations, filename="all_translated_pages.pdf")
     else:
+        delete_processing_message(update, context, processing_msg_id)
         update.message.reply_text('Unsupported document type.')
 
 def send_merged_pdf(update, context, texts, filename="translation.pdf"):
@@ -290,24 +258,24 @@ def menu_language(update: Update, context: CallbackContext) -> None:
 
 def main() -> None:
     updater = Updater(TELEGRAM_API_KEY)
-    
     dispatcher = updater.dispatcher
+
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help))
     dispatcher.add_handler(CommandHandler("ocr_file", ocr_file))
+
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_text))
-    dispatcher.add_handler(MessageHandler(Filters.document.mime_type("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet") & ~Filters.command, handle_excel))
-    dispatcher.add_handler(MessageHandler(Filters.document & ~Filters.command, handle_document))
     dispatcher.add_handler(CallbackQueryHandler(handle_language_selection, pattern="^[a-z]{2}$"))
     dispatcher.add_handler(MessageHandler(Filters.photo & ~Filters.command, handle_image))
     dispatcher.add_handler(MessageHandler(Filters.document & ~Filters.command, handle_document))
     dispatcher.add_handler(CommandHandler("menu_output_format", menu_output_format))
     dispatcher.add_handler(CallbackQueryHandler(handle_format_selection, pattern="^(text|pdf)$"))
     dispatcher.add_handler(CommandHandler("menu_language", menu_language))
-    dispatcher.add_error_handler(error_handler)  # Add the error handler
 
     updater.start_polling()
+
     updater.idle()
 
 if __name__ == '__main__':
     main()
+    
